46d45bf8a8d82e41f2ae55853caaeca43455ebb4
Ezzyyyyy <elliot-thorpe@hotmail.co.uk>
2023-11-01 21:30:43 +0000

    Added window and document built-in variables and functions with explanations of how they work

---
+++ app.js
@@ -0,0 +1,5 @@
+window; //Holds information and functionality related to the active browser window/tab - eg. current location, utility functions
+
+document; //Holds information and functionality to the loaded website content e.g Utilitty functions to access HTML elements
+
+//window.document 
