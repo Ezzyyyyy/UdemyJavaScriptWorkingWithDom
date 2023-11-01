e8f3e240eb61b6d330f2c8602b5ef4bfd4c4875f
Ezzyyyyy <elliot-thorpe@hotmail.co.uk>
2023-11-01 21:42:20 +0000

    Added correct HTML, changed app.js with explanations to explanations.js and created new app.js

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-window; //Holds information and functionality related to the active browser window/tab - eg. current location, utility functions
-
-document; //Holds information and functionality to the loaded website content e.g Utilitty functions to access HTML elements
-
-//window.document 
-
-// Document Object Model
-// The data representation ("internal representation") of the parsed HTML Code
-// The browser parses our HTML code and saves all elements as JavaScript Objects
-// Our JS code is able to interact with the DOM and extract data from it or manipulate it
+console.log(document);
