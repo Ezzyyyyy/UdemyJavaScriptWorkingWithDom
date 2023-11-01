8443473283bd2e45d8b9ac99cb959cc275534632
Ezzyyyyy <elliot-thorpe@hotmail.co.uk>
2023-11-01 22:03:49 +0000

    Added line of code to access anchor element in HTML and change the link through JS

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-console.log(document);
+document.body.children[1].children[0].href = "https://google.com"; // Access body of document, then the child of body, then child of body (p), then anchor element to change link in HTML through JS
