f61b6afcd055b56b42bcad50732f3acda9bcddd8
Ezzyyyyy <elliot-thorpe@hotmail.co.uk>
2023-11-02 13:33:28 +0000

    Added explanation as to how to query elements

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-document.body.children[1].children[0].href = "https://google.com"; // Access body of document, then the child of body, then child of body (p), then anchor element to change link in HTML through JS
+document.body.children[1].children[0].href = "https://google.com"; // Access body of document, 
+//then the child of body, then child of body (p), then anchor element to change link in HTML through JS
+
 //Drilling into the DOM ^^
--- explanation.js
+++ explanation.js
@@ -8,3 +8,7 @@ document; //Holds information and functionality to the loaded website content e.
 // The data representation ("internal representation") of the parsed HTML Code
 // The browser parses our HTML code and saves all elements as JavaScript Objects
 // Our JS code is able to interact with the DOM and extract data from it or manipulate it
+
+// Query Elements
+// document.getElementByID('some-id')
+// document.querySelector('.some-class') - Works like CSS so no exact DOM Structure is required.
