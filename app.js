be5abdf0f2a5ccd4f2bdd9ee3ac5f438d451b6e8
Ezzyyyyy <elliot-thorpe@hotmail.co.uk>
2023-11-03 15:10:35 +0000

    Added event listener that logs the inputted text into the input html element

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ function changeParagraphText() {
 }
 
 paragraphElement.addEventListener("click", changeParagraphText); // No need for () after.
+
+let inputElement = document.querySelector("input");
+
+function retrieveUserInput() {
+    let enteredText = inputElement.value; // .value holds current value inputted
+    console.log(enteredText);
+}
+
+inputElement.addEventListener("input", retrieveUserInput);
