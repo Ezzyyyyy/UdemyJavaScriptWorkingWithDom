db08f9d48e397d83516f2d20c0af1799a08dd2cd
Ezzyyyyy <elliot-thorpe@hotmail.co.uk>
2023-11-03 15:20:29 +0000

    Added different way to get value in function, without using variable outside of function

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ let paragraphElement = document.querySelector("p");
 
 function changeParagraphText() {
   paragraphElement.textContent = "Clicked!";
+  console.log('Paragraph Clicked!');
 }
 
 paragraphElement.addEventListener("click", changeParagraphText); // No need for () after.
 
 let inputElement = document.querySelector("input");
 
-function retrieveUserInput() {
-    let enteredText = inputElement.value; // .value holds current value inputted
+function retrieveUserInput(event) {
+    // let enteredText = inputElement.value; // .value holds current value inputted
+    let enteredText = event.target.value; // Does the same as previous, but doesn't access anything outside of function
     console.log(enteredText);
+    console.log(event);
 }
 
 inputElement.addEventListener("input", retrieveUserInput);
